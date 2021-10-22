130fa0cfec64df2457d89e05d81b691e8b180918
chrisrhayden <chrisrhayden@gmail.com>
2021-10-22 09:02:14 -0700

    code clean up
    adding a parse arg function; no error on bad argument
    small clean up

--- src/main.rs
+++ src/main.rs
@@ -3,65 +3,65 @@
 mod morse_code_key;
 mod morse_printer;
 
-use std::env::args;
+use std::{env::args, error::Error};
 
 use morse_code_key::{ascii_to_morse, morse_to_ascii};
+use morse_printer::{print_morse_code, print_morse_key};
 
-#[derive(Debug, Clone)]
-enum Morse {
-    Dit,
-    Dah,
-    Space,
+struct Args {
+    print_key: bool,
+    message: Option<String>,
 }
 
-impl Morse {
-    fn value(&self) -> u64 {
-        match self {
-            Morse::Dit => 2,
-            Morse::Dah => 3,
-            Morse::Space => 0,
+impl Default for Args {
+    fn default() -> Self {
+        Self {
+            print_key: false,
+            message: None,
         }
     }
 }
 
-fn unpack_morse_value(morse_char: &[u64]) -> char {
+fn convert_to_ascii(morse_char: &[u64]) -> char {
     let mut morse_char_value = 0;
-    for (i, v) in morse_char.iter().enumerate() {
-        if i != 0 {
-            morse_char_value <<= 2;
-        }
+
+    for v in morse_char {
+        morse_char_value <<= 2;
         morse_char_value += v;
     }
 
     morse_to_ascii(morse_char_value)
 }
 
-fn unpack_morse_code(code_vec: Vec<u64>) -> String {
+fn unpack_morse_code(code_vec: &[u64]) -> String {
     let mask = 0b11;
+
     let mut message = String::new();
     let mut morse_char: Vec<u64> = vec![];
 
-    for mut code in code_vec.into_iter().rev() {
+    for code_immutable in code_vec.iter().rev() {
+        let mut code = code_immutable.clone();
+
         while code != 0 {
-            let seq = code & mask;
+            let sequence = code & mask;
+
             code >>= 2;
-            if seq == 0b01 {
+
+            if sequence == 0b01 {
                 if !morse_char.is_empty() {
-                    let ascii_char = unpack_morse_value(&morse_char);
-
+                    let ascii_char = convert_to_ascii(&morse_char);
                     message.push(ascii_char);
-
                     morse_char.clear();
                 }
             } else {
-                morse_char.insert(0, seq);
+                morse_char.insert(0, sequence);
             }
         }
 
         if !morse_char.is_empty() {
-            let ascii_char = unpack_morse_value(&morse_char);
-            morse_char.clear();
+            let ascii_char = convert_to_ascii(&morse_char);
             message.push(ascii_char);
+            morse_char.clear();
         }
     }
 
@@ -70,13 +70,16 @@ fn unpack_morse_code(code_vec: Vec<u64>) -> String {
 
 fn make_morse_code(msg: &str) -> Vec<u64> {
     let mask = 9223372036854775808;
+
     let mut morse_code: Vec<u64> = vec![0];
 
     let mut indx = 0;
 
     let mut code = &mut morse_code[indx];
+
     for c in msg.chars() {
         let (morse_value, offset) = ascii_to_morse(c);
+
         if *code << offset & mask > 0 {
             morse_code.push(0);
             indx += 1;
@@ -89,38 +92,57 @@ fn make_morse_code(msg: &str) -> Vec<u64> {
         *code += 1;
     }
 
-    println!("{:?}", morse_code);
     morse_code
 }
 
-fn main() {
-    let mut print = false;
-    let mut  get_msg = false;
-    let mut msg: Option<String> = None;
-    for arg in args() {
-        if arg == "--print" || arg == "-p" {
-            print = true;
+fn parse_args() -> Result<Args, Box<dyn Error>> {
+    let mut arg_options = Args::default();
+
+    let mut get_msg = false;
+
+    for arg in args().skip(1) {
+        if cfg!(debug_assertions) {
+            dbg!(&arg);
+        }
+
+        if arg == "--print-key" || arg == "-k" {
+            arg_options.print_key = true;
         } else if arg == "--msg" || arg == "-m" {
             get_msg = true;
-        }
-        if get_msg {
-            msg = Some(arg);
+        } else if get_msg {
+            arg_options.message = Some(arg);
             get_msg = false;
+        } else {
+            return Err(Box::from(format!("bad arg -- {}", arg)));
         }
     }
 
-    if print {
-        morse_printer::print_morse_values();
+    if !arg_options.print_key && arg_options.message.is_none() {
+        Err(Box::from(String::from("did not get arguments")))
     } else {
-        if let Some(msg) = msg {
-            let morse_code = make_morse_code(&msg);
-            for num in &morse_code {
-                print!("{:b}", num);
-            }
-            println!();
-            let new_msg = unpack_morse_code(morse_code);
-
-            println!(">{}<", new_msg.chars().rev().collect::<String>());
-        }
+        Ok(arg_options)
     }
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let arg_options = parse_args()?;
+
+    if arg_options.print_key {
+        if cfg!(debug_assertions) {
+            dbg!("print morse code key");
+        }
+
+        print_morse_key();
+    }
+    if let Some(msg) = arg_options.message {
+        if cfg!(debug_assertions) {
+            dbg!(&msg);
+        }
+
+        let morse_code = make_morse_code(&msg);
+        let decoded_msg = unpack_morse_code(&morse_code);
+        print_morse_code(&morse_code, &decoded_msg);
+    }
+
+    Ok(())
 }
